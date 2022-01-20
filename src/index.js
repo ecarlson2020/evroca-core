3afe84b2cb7be150304a60eaf388a4f48138ee21
Evan Carlson <ecarlson2020@gmail.com>
2022-01-20 13:07:38 -0700

    Root component to App

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function Root(){
+function App(){
 	return <div></div>
 }
 
-ReactDOM.render(<Root />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
