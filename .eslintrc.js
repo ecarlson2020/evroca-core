f5124f4d6ce3da1723ff89e7f9edad3f7c5dd89f
Evan Carlson <ecarlson2020@gmail.com>
2022-03-02 22:04:10 -0700

    new rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
     'prettier',
   ],
   rules: {
-    "react/jsx-filename-extension": 0
+    "react/jsx-filename-extension": 0,
+    "react/jsx-props-no-spreading": 0,
   },
 };
