a78df646f090bf4e7bc35139edc5db68d3a05c8f
Evan Carlson <ecarlson2020@gmail.com>
2022-03-21 20:05:04 -0600

    no need for file loader

--- webpack.config.js
+++ webpack.config.js
@@ -16,13 +16,9 @@ module.exports = {
         use: ["style-loader", "css-loader"]
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-          },
-        ],
-      },
+        test: /\.(jpe?g|png|gif|svg)$/,
+        type: 'asset/resource'
+      }
     ]
   },
   plugins: [
