09e35b4983ca954e2e0bcf280c1b27c5e97a7002
Evan Carlson <ecarlson2020@gmail.com>
2022-03-30 15:59:19 -0600

    adding Carousel component

---
+++ src/components/Carousel.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import "./index.css";
+
+const style = {
+  car: {
+    display: "flex",
+    whiteSpace: "nowrap",
+    overflowX: "auto",
+    scrollBehavior: "smooth",
+  },
+  item: {
+    width: "calc(25% - 20px)",
+    whitespace: "normal",
+    padding: "10px",
+  },
+  chevron: {
+    position: "absolute",
+    top: "calc(50% - 35px / 2)",
+    width: "35px",
+    height: "35px",
+    textAlign: "center",
+    borderRadius: "100%",
+    background: "rgba(0,0,0,0.3)",
+    color: "#fff",
+  },
+};
+
+export default function Carousel({ children }) {
+  const [myRef, setMyRef] = useState(null);
+
+  const handleClick = (isLeft) => {
+    const thisMuch = window.innerWidth / 4;
+    if (isLeft) {
+      myRef.scrollLeft -= thisMuch;
+    } else {
+      myRef.scrollLeft += thisMuch;
+    }
+  };
+
+  const drawChevron = (isLeft) => {
+    const conditionalStyle = isLeft ? { left: "15px" } : { right: "15px" };
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        style={{ ...style.chevron, ...conditionalStyle }}
+        onClick={() => handleClick(isLeft)}
+      >
+        <polyline points={isLeft ? "15 18 9 12 15 6" : "9 18 15 12 9 6"} />
+      </svg>
+    );
+  };
+
+  return (
+    <div>
+      <div style={{ position: "relative" }}>
+        {drawChevron(true)}
+        {drawChevron(false)}
+        <div id="evroca-carousel" style={style.car} ref={setMyRef}>
+          {children.map((el, i) => (
+            <div key={i} style={style.item}>
+              {el}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+// PropTypes
+Carousel.propTypes = {
+  children: PropTypes.node.isRequired,
+};
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 function App() {
-  return <div>yeet</div>;
+  return <Carousel>first slide second slide third slide</Carousel>;
 }
 
 ReactDOM.render(<App />, document.querySelector("#root"));
