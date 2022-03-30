f65c77cd560890494f4f138fc0c08bba88a8de96
Evan Carlson <ecarlson2020@gmail.com>
2022-03-30 17:44:27 -0600

    progress

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Carousel from "./components/Carousel";
 
 function App() {
-  return <Carousel>first slide second slide third slide</Carousel>;
+  return (
+    <Carousel>
+      <div>hey</div>
+      <div>hey2</div>
+    </Carousel>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector("#root"));
